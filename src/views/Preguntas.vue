cae628a596827e94d84d7f895e1f4b7fbd9efded
Cristhiam Monsalve <ccmonapa@hotmail.com>
2020-03-16 01:36:55 -0500

    Ajuste numeral y cantidad de preguntas, campo status

--- src/views/Preguntas.vue
+++ src/views/Preguntas.vue
@@ -20,6 +20,7 @@
             </div>    
               <!-- TABLA LISTADO GENERAL -->
             <div  style="max-width: 95%;" class="table-responsive mr-2 ml-2 pl-2 pr-2">
+                <!--<button type="button" @click="ajustar()">Ajustar</button>-->
                 <table class="table table-bordered" style ="font-size: 12px !important; padding: 1em !important;">
                     <thead class="thead-dark" >
                         <th>#</th><th>Pregunta</th><th>Opcion A</th><th>Opcion B</th><th>Opcion C</th><th>Opcion D</th>
@@ -58,6 +59,19 @@
                         </tr>
                     </tbody>
                 </table>
+                <nav>
+                    <ul class="pagination">
+                        <li class="page-item" v-if="pagination.current_page > 1">
+                            <a class="page-link" href="#" @click.prevent="cambiarPagina(pagination.current_page - 1,buscar)">Ant</a>
+                        </li>
+                        <li class="page-item" v-for="page in pagesNumber" :key="page" :class="[page == isActived ? 'active' : '']">
+                            <a class="page-link" href="#" @click.prevent="cambiarPagina(page,buscar)" v-text="page"></a>
+                        </li>
+                        <li class="page-item" v-if="pagination.current_page < pagination.last_page">
+                            <a class="page-link" href="#" @click.prevent="cambiarPagina(parseInt(pagination.current_page) + 1,buscar)">Sig</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
      </div>
@@ -83,11 +97,52 @@ import "firebase/firestore";
                 nivel: '',
                 buscar: 'xx',
                 pagina: '',
-                counter: 0
+                size: 10,
+                counter: 0,
+                contador:0,
+                offset : 10,
+                pagination : {
+                    'total' : 50,
+                    'current_page' : 1,                    
+                    'last_page' : 50,
+                    'from' : 1,
+                    'to' : 10,
+                }
             }    
         },
+       
+         computed:{
+            isActived: function(){
+                return this.pagination.current_page;
+            },
+            //Calcula los elementos de la paginación
+            pagesNumber: function() {
+                if(!this.pagination.to) {
+                    return [];
+                }
+                
+                var from = this.pagination.current_page - this.offset; 
+                if(from < 1) {
+                    from = 1;
+                }
+
+                var to = from + (this.offset * 2); 
+                if(to >= this.pagination.last_page){
+                    to = this.pagination.last_page;
+                }  
+
+                var pagesArray = [];
+                while(from <= to) {
+                    pagesArray.push(from);
+                    from++;
+                }
+                return pagesArray;             
+
+            }
+        },
+
         methods : {
-            show(buscar){
+            show(pagina,buscar){
                 var db = firebase.firestore();
                 let me = this;
                 console.log(buscar);
@@ -98,10 +153,13 @@ import "firebase/firestore";
                     })
                     //console.log(querySnapshot);
                 });*/
-                db.collection('preguntas_nivel1').orderBy('numeral').get()
-                .then((snapshot) => {
+                
+                db.collection('preguntas_nivel1').orderBy('numeral').limit( me.offset).startAfter(10).get()
+                .then((snapshot) => {                      
+                    //console.log(snapshot);
                     snapshot.forEach((doc) => {                        
                         me.lista_preguntas.push(doc.data());
+                        
                     });
                     //console.log(me.lista_preguntas);
                 })
@@ -109,7 +167,45 @@ import "firebase/firestore";
                     console.log('Error getting documents', err);
                 });
             },
+            ajustar(){
+                var db = firebase.firestore();
+                let me = this;
+                db.collection('preguntas_nivel3').orderBy('numeral').limit( 3000).get()
+                .then((snapshot) => {   
+                    me.contador = 0;
+                    console.log(snapshot);
+                    snapshot.forEach((doc) => {               
+                        if(doc.data().numeral>500){
+                         // db.collection('preguntas_nivel3').doc(doc.id).delete()
+                        }         
+                        /*else{
+                            console.log(doc.data().numeral+" estado = "+doc.data().estado)
+                        }
+                       // me.lista_preguntas.push(doc.data());
+                        //console.log(doc.data().numeral);
+                      //  me.lista_preguntas.push(doc.data());
+                      /*  me.contador ++;
+                        db.collection("preguntas_nivel4").doc(doc.id).set({
 
+                            numeral: me.contador,
+                            opcion_a: doc.data().opcion_a,
+                            opcion_b: doc.data().opcion_b,
+                            opcion_c: doc.data().opcion_c,
+                            opcion_d: doc.data().opcion_d,
+                            pista: doc.data().pista,
+                            pregunta: doc.data().pregunta,
+                            respuesta: doc.data().respuesta,
+                            estado: "1"
+                            
+                        })
+                       //console.log(doc);*/
+                    });
+                    //console.log(me.lista_preguntas);
+                })
+                .catch((err) => {
+                    console.log('Error getting documents', err);
+                });
+            },
             limpiar(){
 
             },
@@ -125,12 +221,19 @@ import "firebase/firestore";
             }, 
             inactivate(id_preg){
                 console.log(id_preg);
+            },
+            cambiarPagina(page,buscar){
+                let me = this;
+                //Actualiza la página actual
+                me.pagination.current_page = page;
+                //Envia la petición para visualizar la data de esa página
+                me.show(page,buscar);
             }
         }
         ,
         mounted() {               
             //const admin = admin.initializeApp();
-            this.show();
+            this.show(1,this.buscar);
             //this.listar_usuarios();            
         }
     }
